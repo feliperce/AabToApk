3aab09599992257886a7a7ad78503fcdd4f142c6
Felipe Celestino <felipe.rce@gmail.com>
2024-08-15 23:03:54 -0300

    added unzip fun

--- composeApp/src/commonMain/kotlin/utils/ApkExtractor.kt
+++ composeApp/src/commonMain/kotlin/utils/ApkExtractor.kt
@@ -4,9 +4,15 @@ import com.android.tools.build.bundletool.androidtools.Aapt2Command
 import com.android.tools.build.bundletool.commands.BuildApksCommand
 import com.android.tools.build.bundletool.model.Password
 import com.android.tools.build.bundletool.model.SigningConfiguration
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.nio.file.Paths
 import java.util.*
+import java.util.zip.ZipFile
 
 class ApkExtractor(
     private val aabPath: String,
@@ -52,7 +58,7 @@ class ApkExtractor(
             if (signingConfig != null) {
                 runCatching {
                     val newApksFileName = apksFileName.ifEmpty {
-                        "extracted.apks"
+                        "extracted"
                     }
 
                     val formattedOutputPath = outputApksPath.dropLastWhile { it == '/' }
@@ -69,8 +75,14 @@ class ApkExtractor(
                         .setAapt2Command(Aapt2Command.createFromExecutablePath(Paths.get(aapt2Path)))
                         .setOverwriteOutput(overwriteApks)
                         .setOutputFile(Paths.get(formattedOutputPath))
+                        .setApkBuildMode(BuildApksCommand.ApkBuildMode.UNIVERSAL)
                         .build()
                         .execute()
+
+                    unzipUniversalApks(
+                        formattedOutputPath,
+                        newApksFileName
+                    )
 
                     onSuccess(formattedOutputPath)
                 }.onFailure { failure ->
@@ -83,5 +95,33 @@ class ApkExtractor(
                 }
             }
         }.await()
+    }
+
+    private suspend fun unzipUniversalApks(
+        apksPath: String,
+        apksFileName: String
+    ): String {
+        return withContext(Dispatchers.IO) {
+            val zipFile = ZipFile(apksPath)
+            val universalApkEntry = zipFile.entries().asSequence()
+                .find { it.name == "universal.apk" }
+
+            universalApkEntry?.let { entry ->
+                zipFile.getInputStream(entry).use { input ->
+                    val outputFilePath = outputApksPath.trimEnd('/') + "/$apksFileName-(universal).apk"
+                    val outputFile = File(outputFilePath)
+
+                    outputFile.outputStream().buffered().use { output ->
+                        input.copyTo(output, bufferSize = 4096)
+                    }
+
+                    File(apksPath).delete()
+
+                    return@withContext outputFile.absolutePath
+                }
+            }
+
+            return@withContext ""
+        }
     }
 }
